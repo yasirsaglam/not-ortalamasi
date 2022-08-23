021d81f3dc868620eaa91c263061d36b711550f3
yasirsaglam <yasirsaglamm@gmail.com>
2022-08-23 22:25:30 +0300

    Proje tamamlandı.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
+        int mat, turkce, tarih, muzik, fizik, kimya;
+
+        Scanner giris = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu giriniz : ");
+        mat = giris.nextInt();
+
+        System.out.println("Türkçe notunuzu giriniz : ");
+        turkce = giris.nextInt();
+
+        System.out.println("Tarih notunuzu giriniz : ");
+        tarih = giris.nextInt();
+
+        System.out.println("Müzik notunuzu giriniz : ");
+        muzik = giris.nextInt();
+
+        System.out.println("Fizik notunuzu giriniz : ");
+        fizik = giris.nextInt();
+
+        System.out.println("Kimya notunuzu giriniz : ");
+        kimya = giris.nextInt();
+
+        int toplam = (mat + tarih + turkce + fizik + muzik + kimya);
+        double ort = toplam / 6;
+
+        System.out.println("Not Ortalamanız : " + ort);
 
     }
 }
